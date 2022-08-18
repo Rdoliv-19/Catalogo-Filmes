a718d1214d460ec3723dfcb082b66edf8fc7f5e8
Rodrigo <digo.s.oliv@gmail.com>
2022-08-17 21:55:01 -0300

    if função filmes

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 let filme = document.getElementById('filme').value
+
+if(filmeFavorito.endsWith(".jpg")){
+    listaFilmesnaTela(filmeFavorito)
+   } else{
+    console.error("Endereço de filme inválido")
+  }
+    document.getElementById("filme").value = ""
