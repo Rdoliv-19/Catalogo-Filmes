5bb1e123785c158e141d94d065a122f36111bf2c
Rodrigo <digo.s.oliv@gmail.com>
2022-08-18 21:15:18 -0300

    Função lista filmes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+function adicionarFilme() {
 let filme = document.getElementById('filme').value
 
 if(filmeFavorito.endsWith(".jpg")){
@@ -6,3 +8,11 @@ if(filmeFavorito.endsWith(".jpg")){
     console.error("Endereço de filme inválido")
   }
     document.getElementById("filme").value = ""
+}
+
+function listaFilmesnaTela(filme){
+  var elementoFilmeFavorito = "<img src="  + filme + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+   elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML+          elementoFilmeFavorito;
+  
+}
